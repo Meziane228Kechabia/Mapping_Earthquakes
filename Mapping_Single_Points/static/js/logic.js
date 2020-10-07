eaed7bae83ca66a4e326882c823cc8615bc02db2
Meziane Kechabia <meziane228@gmail.com>
2020-10-06 23:17:57 -0700

    Adding circle radius of Los Angeles

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,11 +14,22 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //   });
 
 // Create the tile layer that will be the backround of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API
 });
 
+// Add a Marker to the Map for Los Angeles, California.
+let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map
 streets.addTo(map);
+
