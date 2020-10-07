52ac55258b8854418cf9b2b79fc0832fd074663e
Meziane Kechabia <meziane228@gmail.com>
2020-10-07 13:27:21 -0700

    mapping line with 4 points

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,68 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// // We can also use the code below when using backround image or multiple tile
+// // Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+// Create the tile layer that will be the backround of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API
+});
+
+
+
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
+
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// // Add a circle to the map
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map
+streets.addTo(map);
+
+// // Get data from mcities.js
+// let cityData = cities;
+
+// // Loop throught the cities array and create one marker for each city.
+// citiData.forEach(function(city) {
+//     console.log(city)
+// });
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
