5af0c6d35569bf8f0773ce47a9170495cd22beca
Meziane Kechabia <meziane228@gmail.com>
2020-10-07 20:23:12 -0700

    Adding the majorAirports.json

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -14,10 +14,10 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //   });
 
 // Create the tile layer that will be the backround of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    accessToken: API
 });
 
 // Then we add our 'graymap' tile layer to the map
